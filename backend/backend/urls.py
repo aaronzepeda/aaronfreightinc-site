f82537ee092dbb90867b36de4897410c4860ef20
Aaron Zepeda <a1zepeda@ucsd.edu>
2023-05-24 20:41:10 -0700

    Fixed NGINX serving

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     # Dev-only Serve
-    urlpatterns += static('/static_root/', document_root=settings.STATIC_ROOT)
-    urlpatterns += static('/media/', document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
