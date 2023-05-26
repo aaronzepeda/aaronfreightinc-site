dd209ff443ec60568c92c5069269b0f87180547e
Aaron Zepeda <a1zepeda@ucsd.edu>
2023-05-25 18:24:15 -0700

    Fixed NGINX

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -152,10 +152,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / 'static'
-STATIC_URL = 'static/'
+STATIC_URL = 'backend/static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = 'media/'
+MEDIA_URL = 'backend/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
